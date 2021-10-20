77f991357ba4302b79584742de88a6f0389ed9b7
pedro <pdemeu@outlook.com>
2021-10-20 07:43:06 -0300

    melhorando o codigo e componente sendmessageform

--- src/components/LoginBox/index.tsx
+++ src/components/LoginBox/index.tsx
@@ -1,15 +1,18 @@
 import { VscGithubInverted } from 'react-icons/vsc'
+
+import { useAuth } from '../../hooks/useAuth';
 import styles from './styles.module.scss';
 
-
 export function LoginBox() {
-    return (
-        <div className={styles.loginBoxWrapper}>
-            <strong>Entre e compartilhe sua mensagem</strong>
-            <a href="#" className={styles.signInWithGithub}>
-                <VscGithubInverted size="24" />
-                Entrar com Github
-            </a>
-        </div>
-    )
+  const { signInUrl } = useAuth()
+
+  return (
+    <div className={styles.loginBoxWrapper}>
+      <strong>Entre e compartilhe sua mensagem</strong>
+      <a href={signInUrl} className={styles.signWithGithubButton}>
+        <VscGithubInverted size={24} />
+        Entrar com Github
+      </a>
+    </div>
+  )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {App} from './App'
+import { App } from './App'
+import { AuthProvider } from './hooks/useAuth'
 import './styles/global.css'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </React.StrictMode>,
   document.getElementById('root')
 )
